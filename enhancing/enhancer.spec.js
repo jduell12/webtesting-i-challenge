4c9929edd698090d5defbdc761ea9c03eeed8d91
Jessica <duellje@oregonstate.edu>
2020-08-19 14:36:03 -0400

    added test for repair method

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,21 @@
-const enhancer = require('./enhancer.js');
-// test away!
+const enhancer = require("./enhancer.js");
+
+const item = {
+  name: "sword",
+  durability: 80,
+  enhancement: 15,
+};
+
+describe("repair item", () => {
+  it("returns a new item with durability restored to 100", () => {
+    const expectedItem = {
+      name: "sword",
+      durability: 100,
+      enhancement: 15,
+    };
+
+    expect(enhancer.repair(item)).toStrictEqual(expectedItem);
+    expect(enhancer.repair(item)).not.toStrictEqual(item);
+    expect(enhancer.repair(item)).not.toStrictEqual({});
+  });
+});
